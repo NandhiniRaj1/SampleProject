e72cea60313eefa7da4752493234b9818ecbd81b
Nandhini Raj <nandh.raj@gmail.com>
2021-11-09 16:14:46 +0530

    First Commit for this project

---
+++ src/main/java/Customer.java
@@ -0,0 +1,16 @@
+public class Customer {
+    private String name;
+    private int customerId;
+
+
+    Customer()
+    {
+        name = "Hello";
+        id=12;
+    }
+
+    public String toString()
+    {
+        System.out.println("Customer Name" + customer + "CustomerID" + customerId);
+    }
+}
---
+++ src/main/java/DemoApplication.java
@@ -0,0 +1,9 @@
+public class DemoApplication {
+
+    public static void main(String args[])
+    {
+        Customer cust = new Customer();
+
+        cust.toString();
+    }
+}
